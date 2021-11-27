d475ef9b5d1a5d1ca88981e08db0443df245232e
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-11-27 21:17:45 +0300

    nested table view is finished.

--- Nested TableView/Nested TableView/ViewController.swift
+++ Nested TableView/Nested TableView/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    var path = ["Fruits","Colors","Seasons"]
+    var pathData = [["Apple","Banana","Strawberry"],["Red","Blue","Purple"],["Summer","Winter","Spring","Autumn"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
 
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return path.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return path[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pathData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell",for: indexPath)
+        cell.textLabel?.text = pathData[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(path[indexPath.section]) - > \(pathData[indexPath.section][indexPath.row])")
+    }
 }
 
